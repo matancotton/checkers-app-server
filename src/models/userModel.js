fdc481b8be73188c015566927ef0915fd8e68d4e
matancotton <matancotton1@gmaol.com>
2020-12-22 22:40:14 +0200

    update jwt validation

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -77,7 +77,8 @@ userSchema.statics.findByCredentials = async (username,password)=>{
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET )
+    const token = jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET,
+    {expiresIn: "2h"} )
     user.tokens = user.tokens.concat({token})
     await user.save()
     return token
@@ -87,7 +88,7 @@ userSchema.methods.toJSON = function() {
     const user = this
     const userObject = user.toObject()
     delete userObject.password
-    delete userObject.tokens
+    delete userObject.score
     delete userObject.avatar
     return userObject
 }
