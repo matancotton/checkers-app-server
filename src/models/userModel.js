2b3c1d9863704603b23f3d6a26c57f112674e5b8
matancotton <matancotton1@gmaol.com>
2021-04-13 19:25:48 +0300

    delete tokens from userData

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema(
     {
         username: {
@@ -11,9 +11,9 @@ const userSchema = new mongoose.Schema(
             unique: true,
             validate(value) {
                 if (value.trim().length < 0) {
-                    throw new Error('must enter a valid username')
+                    throw new Error("must enter a valid username");
                 }
-            }
+            },
         },
         password: {
             type: String,
@@ -22,10 +22,12 @@ const userSchema = new mongoose.Schema(
             minlength: 6,
             validate(value) {
                 const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{0,}$/;
-				if (!passRegex.test(value)) {
-					throw new Error("password must contain big and small characters and numbers");
-				}
-            }
+                if (!passRegex.test(value)) {
+                    throw new Error(
+                        "password must contain big and small characters and numbers"
+                    );
+                }
+            },
         },
         email: {
             type: String,
@@ -35,73 +37,76 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             validate(value) {
                 if (!validator.isEmail(value)) {
-                    throw new Error('Invalid email')
+                    throw new Error("Invalid email");
                 }
-            }
+            },
         },
         age: {
             type: Number,
             min: 12,
-            required: true
+            required: true,
         },
         score: {
             type: Number,
-            default: 0
+            default: 0,
         },
-        tokens: [{ 
-            token:{
-            type: String,
-            required: true
-        }
-            
-        }]
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
     },
     {
-        timestamps: true
+        timestamps: true,
     }
-)
+);
 
-userSchema.statics.findByCredentials = async (username,password)=>{
-    const user = await User.findOne({username})
+userSchema.statics.findByCredentials = async (username, password) => {
+    const user = await User.findOne({ username });
     if (!user) {
-        throw new Error('username or password is incorrect')
+        throw new Error("username or password is incorrect");
     }
 
-    const isMatch = await bcrypt.compare(password,user.password)
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        throw new Error('username or password is incorrect')
+        throw new Error("username or password is incorrect");
     }
 
-    return user
-}
+    return user;
+};
 
 userSchema.methods.generateAuthToken = async function () {
-    const user = this
-    const token = jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET,
-    {expiresIn: "2h"} )
-    user.tokens = user.tokens.concat({token})
-    await user.save()
-    return token
-}
+    const user = this;
+    const token = jwt.sign(
+        { _id: user._id.toString() },
+        process.env.JWT_SECRET,
+        { expiresIn: "2h" }
+    );
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+};
 
-userSchema.methods.toJSON = function() {
-    const user = this
-    const userObject = user.toObject()
-    delete userObject.password
-    delete userObject.score
-    delete userObject.avatar
-    return userObject
-}
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.score;
+    delete userObject.tokens;
+    return userObject;
+};
 
-userSchema.pre('save', async function (next) {
-    const user = this
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
     }
-    next()
-})
+    next();
+});
 
-
-const User = mongoose.model('User',userSchema)
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
